cf7868e84f0f6c28930abbc5a8d0c784eb27d422
Jenny <lisalthe@gmai.com>
2024-07-17 01:35:31 +0300

    second changes

--- javascript.js
+++ javascript.js
@@ -10,13 +10,28 @@ const personalMovieDB = {
 	private: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+for (let i = 1; i < 3; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movie[a] = [b];
-personalMovieDB.movie[c] = [d];
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movie[a] = [b];
+		console.log('done');
+
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
 
